b15d1d2ef956501ace6650eddfd115b32c5a27dd
S Sejal Naidu <snaidu@adobe.com>
2024-08-28 15:56:25 +0530

    init

---
+++ creativecloud/blocks/community-block/community-block.js
@@ -0,0 +1,8 @@
+
+export default async function init(el) {
+    const res = await fetch('https://main--milo--adobecom.hlx.page/drafts/snaidu/community/book1.json');
+    const {data} = await res.json();
+    data.forEach(element => {
+        console.log(element);
+    });
+}
